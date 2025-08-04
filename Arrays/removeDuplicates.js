2389a08c364f6633e0675e3c78bb1dcdc1890ebc
Jayashree <s.jayashree94@gmail.com>
2025-08-04 05:46:49 +0530

    first commit

---
+++ Arrays/removeDuplicates.js
@@ -0,0 +1,12 @@
+const removeDuplicates = (arr) => {
+  let x = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > arr[x]) {
+      x++;
+      arr[x] = arr[i];
+    }
+  }
+  return arr;
+};
+
+console.log(removeDuplicates([1, 2, 3, 3, 3, 3, 4, 5, 5, 6, 6, 7]));
