133ea2d4139f34d752086baf1d107d29346b546f
Jayashree <s.jayashree94@gmail.com>
2025-08-17 05:34:00 +0530

    react-interview

--- Interview/memoize.js
+++ Interview/memoize.js
@@ -1,15 +1,28 @@
-import { useState } from "react"
+import { useState, useMemo } from "react"
 
 const memoize =() =>{
 
-    const[Text, setText] = useState("");
+    const[num, setNum] = useState(0);
+    const [theme, setTheme] = useState(false);
 
-
+    const prime = useMemo(() => {findPrime(num), [num]}) 
+    // const prime = findPrime(num);
     return(
-        <input type="text" value ={Text} onChange={(e) => setText(e.target.value)}>
+        <div className={"m-4 p-2" + (theme && "bg-gray=900")}>
+        <input type="number" value ={num} onChange={(e) => setText(e.target.value)}>
             
         </input>
+        <div>
+            <button onClick={() => {setTheme(!theme)}}></button>
+        </div>
+
+        <div>
+            <h1>nth prime : {prime} </h1>
+        </div>
+        </div>
     )
 
 
 }
+
+export default memoize;
