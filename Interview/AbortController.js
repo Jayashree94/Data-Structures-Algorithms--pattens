8a4226ff855fb115dffa8d368d4162f6d94243b0
Jayashree <s.jayashree94@gmail.com>
2025-08-20 06:36:45 +0530

    abortcontroller

---
+++ Interview/AbortController.js
@@ -0,0 +1,17 @@
+async function fetchWithTimeout(url, options = {}, timeout = 5000) {
+    const controller = new AbortController();
+    const id = setTimeout(() => controller.abort(), timeout); 
+
+    try {
+    const response = await fetch(url, {
+      ...options,
+      signal: controller.signal, // attach signal to fetch
+    });
+    return response;
+  } catch (error) {
+    if (error.name === "AbortError") {
+      throw new Error("Request timed out");
+    }
+    throw error;
+  }
+}
