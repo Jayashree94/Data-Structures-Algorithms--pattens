763c9cfb352a735b1acd4463d9fc40faf69255f0
Jayashree <s.jayashree94@gmail.com>
2025-08-21 22:42:43 +0530

    twosum

---
+++ Arrays/Twosum.js
@@ -0,0 +1,14 @@
+var twoSum = function(arr, target) {
+   n = arr.length;
+   let map ={};
+   for(let i =0;i<n;i++){
+    map[arr[i]] = i;
+   }
+
+   for(let i =0;i<n;i++){
+    let pairToFind = target - arr[i];
+    if(map[pairToFind] && map[pairToFind] != i){
+        return [i,map[pairToFind]];
+    }
+   }
+};
